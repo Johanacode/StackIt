16ad6dac8fae0627856e6cba8b9add3b7c4eaf17
Johanacode <117836785+Johanacode@users.noreply.github.com>
2025-07-12 17:30:33 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,11 +1,19 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+// src/firebase/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBuLcSwAlUnuRm-4ytfl9QhIzjYoTwp2Hw",
+  authDomain: "stackit-fe1de.firebaseapp.com",
+  projectId: "stackit-fe1de",
+  storageBucket: "stackit-fe1de.firebasestorage.app",
+  messagingSenderId: "629572135490",
+  appId: "1:629572135490:web:9c786104178123781b9bdb",
+  measurementId: "G-WR2WDQZNPR"
+};
 
-const db = admin.firestore();
-const auth = admin.auth();
-
-module.exports = { db, auth };
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
