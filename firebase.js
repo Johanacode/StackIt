bfc49d1f5c272dde0e1a616f481499ccb781d3f3
Johanacode <117836785+Johanacode@users.noreply.github.com>
2025-07-14 13:32:38 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,1 @@
-// src/firebase/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBuLcSwAlUnuRm-4ytfl9QhIzjYoTwp2Hw",
-  authDomain: "stackit-fe1de.firebaseapp.com",
-  projectId: "stackit-fe1de",
-  storageBucket: "stackit-fe1de.firebasestorage.app",
-  messagingSenderId: "629572135490",
-  appId: "1:629572135490:web:9c786104178123781b9bdb",
-  measurementId: "G-WR2WDQZNPR"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
